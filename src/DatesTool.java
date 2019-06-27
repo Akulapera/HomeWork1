cffe21d7beb7a488c589c861af678595edba9b57
Akulapera <porokhnia.anastasiya@gmail.com>
2019-06-27 21:54:30 +0300

    Added 2 methods (date-> string, string->date)and tests for them.

--- src/DatesTool.java
+++ src/DatesTool.java
@@ -1,33 +1,59 @@
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public class DatesTool {
+public class DatesTool {
 
-        public static LocalDateTime currentDate= LocalDateTime.now();
+    public static LocalDateTime currentDate = LocalDateTime.now();
 
-        public  static  void setCurrentDate (int year, int month, int dayOfMonth, int hour, int minute, int second){
-            currentDate=LocalDateTime.of(year, month, dayOfMonth, hour,  minute, second);
-        }
+    public static void setCurrentDate(int year, int month, int dayOfMonth, int hour, int minute, int second) {
+        currentDate = LocalDateTime.of(year, month, dayOfMonth, hour, minute, second);
+    }
 
-        public static LocalDateTime addHoursToNow(long hoursToAdd) {
-            return currentDate.plusHours(hoursToAdd);
-        }
+    public static LocalDateTime addHoursToNow(long hoursToAdd) {
+        return currentDate.plusHours(hoursToAdd);
+    }
 
-        public static LocalDateTime addMinutesToNow(long minutesToAdd) {
-            return currentDate.plusMinutes(minutesToAdd);
-        }
+    public static LocalDateTime addMinutesToNow(long minutesToAdd) {
+        return currentDate.plusMinutes(minutesToAdd);
+    }
 
-        public static LocalDateTime addDaysToNow(long daysToAdd) {
-            return currentDate.plusDays(daysToAdd);
-        }
+    public static LocalDateTime addDaysToNow(long daysToAdd) {
+        return currentDate.plusDays(daysToAdd);
+    }
 
-        public static LocalDateTime getDatesOrder(LocalDateTime date1, LocalDateTime date2, boolean isEarliest) {
-            boolean isAscendingOrder = date1.isBefore(date2);
-            if (isEarliest && isAscendingOrder || !isEarliest && !isAscendingOrder) {
-                return date1;
-            } else {
-                return date2;
-            }
+    public static LocalDateTime getDatesOrder(LocalDateTime date1, LocalDateTime date2, boolean isEarliest) {
+        boolean isAscendingOrder = date1.isBefore(date2);
+        if (isEarliest && isAscendingOrder || !isEarliest && !isAscendingOrder) {
+            return date1;
+        } else {
+            return date2;
         }
     }
+
+    public static String LocalDateTimeToString(LocalDateTime date) {
+
+        final String DATE_FORMATTER = "yyyy-MM-dd HH:mm:ss";
+        LocalDateTime localDateTime = LocalDateTime.of(2019, 6, 26, 20, 40, 55);
+
+        //System.out.println("Current Time " + localDateTime);  - for checking this line
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMATTER);
+        String formatDateTime = localDateTime.format(formatter);
+
+        //System.out.println("Formatted Time: " +formatDateTime); - for checking this line
+        return formatDateTime;
+    }
+
+    public static LocalDateTime convertStringToLocalDateTime(String stringDate) {
+        String dateTime = "2019-06-26 20:40:55";
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime formatDateTime = LocalDateTime.parse(dateTime, formatter);
+
+        //System.out.println("Parsed Date : " + formatDateTime);
+        return formatDateTime;
+    }
+
+}
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,45 @@
 import java.time.LocalDateTime;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
     public class Main {
 
-        @Test
-        public void  formatDatesTest(){
-            DatesTool.setCurrentDate(2018,03,18,11, 55, 20);
-            LocalDateTime resDate;
-             resDate=LocalDateTime.now();
-            System.out.println("Now is " +resDate);
+        static LocalDateTime dateForConverting = LocalDateTime.of(2019, 6, 26, 20, 40, 55);
 
-            resDate= DatesTool.addHoursToNow(1);
+
+        @Test
+        public void formatDatesTest() {
+            DatesTool.setCurrentDate(2018, 03, 18, 11, 55, 20);
+            LocalDateTime resDate;
+            resDate = LocalDateTime.now();
+            System.out.println("Now is " + resDate);
+
+            resDate = DatesTool.addHoursToNow(1);
             System.out.println("addHoursToNow " + resDate);
 
-            resDate= DatesTool.addMinutesToNow(5);
+            resDate = DatesTool.addMinutesToNow(5);
             System.out.println("addMinutesToNow " + resDate);
 
-            resDate= DatesTool.addDaysToNow(30);
+            resDate = DatesTool.addDaysToNow(30);
             System.out.println("addDaysToNow " + resDate);
         }
+
         @Test
-        public  void  DatesToolAddTest(){
-            DatesTool.setCurrentDate(2018,06,18,11, 55, 20);
-            assertEqualsDate(LocalDateTime.of(2018,03,18,12, 55, 20),
+        public void DatesToolAddTest() {
+            DatesTool.setCurrentDate(2018, 06, 18, 11, 55, 20);
+            assertEqualsDate(LocalDateTime.of(2018, 06, 18, 12, 55, 20),
                     DatesTool.addHoursToNow(1));
         }
 
         @Test
-        public void DatesToolCompairToTest(){
-           LocalDateTime first= LocalDateTime.of(2019,8,18,12, 55, 20);
-           LocalDateTime second=LocalDateTime.of(2019,07,10,14, 11, 07);
-            LocalDateTime third=LocalDateTime.of(2019,07,10,14, 11, 07);
-           assertEqualsDate(DatesTool.getDatesOrder(first, second,true),third);
+        public void DatesToolCompairToTest() {
+            LocalDateTime first = LocalDateTime.of(2019, 8, 18, 12, 55, 20);
+            LocalDateTime second = LocalDateTime.of(2019, 07, 10, 14, 11, 07);
+            LocalDateTime third = LocalDateTime.of(2019, 07, 10, 14, 11, 07);
+            assertEqualsDate(DatesTool.getDatesOrder(first, second, true), third);
         }
 
-        public static  void assertEqualsDate (LocalDateTime firstDate, LocalDateTime secondDate){
+        private static void assertEqualsDate(LocalDateTime firstDate, LocalDateTime secondDate) {
             Assert.assertEquals(firstDate.getYear(), secondDate.getYear());
             Assert.assertEquals(firstDate.getMonthValue(), secondDate.getMonthValue());
             Assert.assertEquals(firstDate.getDayOfMonth(), secondDate.getDayOfMonth());
@@ -45,6 +48,24 @@ import org.testng.annotations.Test;
             Assert.assertEquals(firstDate.getSecond(), secondDate.getSecond());
         }
 
+        @Test
+        public static void convertingLocalDateTimeToStringTest() {
+            String result = DatesTool.LocalDateTimeToString(dateForConverting);
+            //System.out.println(" string is " + result); - for checking this line
+            Assert.assertEquals(result, "2019-06-26 20:40:55");
+            Assert.assertNotEquals(result,"2000-06-26 20:40:55");
+        }
+        @Test
+        public static void convertStringToLocalDateTimeTest(){
+          LocalDateTime result=DatesTool.convertStringToLocalDateTime("2019-06-26 20:40:55");
+          Assert.assertTrue(result.isEqual(dateForConverting));
+          Assert.assertFalse(result.isEqual(dateForConverting.plusHours(4)));
+
     }
+
+    }
+
+
+
 
 
